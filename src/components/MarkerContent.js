938dc68d36faab986e252ec2f219b9210669be6c
Mario555 <mario.magomedov@olimpwarehousing.com>
2023-10-25 17:00:48 -0500

    update

--- src/components/MarkerContent.js
+++ src/components/MarkerContent.js
@@ -1,43 +1,81 @@
 import React from 'react'
+import Icon from 'react-native-vector-icons/FontAwesome'
+import AntDesign from 'react-native-vector-icons/AntDesign'
+
 import { Alert, Image, View, Text, Dimensions, StyleSheet } from 'react-native'
 import { Callout, PROVIDER_GOOGLE } from 'react-native-maps'
 
-import { Paragraph, Avatar, Divider, Button, useTheme } from 'react-native-paper'
+import {
+	Paragraph,
+	Avatar,
+	Divider,
+	Button,
+	useTheme,
+} from 'react-native-paper'
 import { useNavigation } from '@react-navigation/native'
 
-import { sliceStringFilter } from '../utils/filters'
+import {
+	calculateDaysAgo,
+	formatDateHandler,
+	sliceStringFilter,
+} from '../utils/filters'
 import { isImageExist } from '../utils/filters/uploadImageFilter'
 
 const screen = Dimensions.get('screen')
 const date = new Date().toLocaleDateString('en-US')
 
 export default function MarkerContent({ post }) {
+	console.log(post)
+
 	const { colors } = useTheme()
 	const navigation = useNavigation()
 	return (
 		<Callout
 			style={styles.callout}
 			onPress={() => {
-				navigation.navigate('post', { postId: post?._id, postedBy: post?.postedBy?._id })
-			}}>
-			<Text style={{ ...styles.title, color: colors.primary }}>{sliceStringFilter(post?.title, 20)}</Text>
+				navigation.navigate('post', {
+					postId: post?._id,
+					postedBy: post?.postedBy?._id,
+				})
+			}}
+		>
+			<Text style={{ ...styles.title, color: colors.primary }}>
+				{sliceStringFilter(post?.title, 20)}
+			</Text>
 			<Divider />
 			<View style={styles.contentContainer}>
 				<View style={styles.leftSide}>
 					<View style={{ alignContent: 'center' }}>
-						<Text style={{ ...styles.text, color: colors.primary }}>Date: {date}</Text>
+						<Text style={{ ...styles.text, color: colors.primary }}>
+							Created: {calculateDaysAgo(post?.createdAt)}
+						</Text>
 						<Paragraph style={{ ...styles.text, color: colors.primary }}>
 							Price: <Text style={{ fontWeight: '800' }}>{post?.price} $</Text>
 						</Paragraph>
-						<Text style={{ flexShrink: 1, color: colors.primary }}>{sliceStringFilter(post?.description, 50)}</Text>
-						<Button color={colors.blue} compact style={{ marginTop: 10 }} mode='text'>
+						<Text style={{ flexShrink: 1, color: colors.primary }}>
+							{sliceStringFilter(post?.description, 50)}
+						</Text>
+						<Button
+							textColor={colors.blue}
+							compact
+							contentStyle={{ flexDirection: 'row-reverse' }}
+							style={{ marginTop: 10 }}
+							mode="outlined"
+							icon={() => (
+								<AntDesign name="forward" color={colors.blue} size={14} />
+							)}
+							name="prev"
+						>
 							More...
 						</Button>
 					</View>
 				</View>
 				<View style={styles.rightSide}>
 					{/* <Avatar.Text size={55} label={'JD'} /> */}
-					<Image style={{ height: 120, width: '100%', borderRadius: 8 }} source={{ uri: isImageExist(post?.images) }} />
+					<Image
+						style={{ height: 120, width: '100%', borderRadius: 8 }}
+						source={{ uri: isImageExist(post?.images) }}
+					/>
 				</View>
 			</View>
 		</Callout>
--- src/utils/filters/formatDate.js
+++ src/utils/filters/formatDate.js
@@ -6,7 +6,43 @@
 export const formatDateHandler = (currDate) => {
 	if (!currDate) return
 	const today = new Date(currDate)
-	const options = { weekday: 'long', year: 'numeric', month: 'short', day: 'numeric' }
+	const options = {
+		weekday: 'long',
+		year: 'numeric',
+		month: 'short',
+		day: 'numeric',
+	}
 	const date = today.toLocaleDateString('en-En', options)
 	return date
+}
+
+export const calculateDaysAgo = (date) => {
+	const createdDate = new Date(date)
+	const currentDate = new Date()
+
+	const timeDifference = currentDate - createdDate
+	const millisecondsPerDay = 1000 * 60 * 60 * 24
+
+	const totalDays = Math.floor(timeDifference / millisecondsPerDay)
+	const totalYears = Math.floor(totalDays / 365)
+	const remainingDays = totalDays % 365
+
+	const totalMonths = Math.floor(remainingDays / 30)
+	const remainingDaysInMonth = remainingDays % 30
+
+	const result = []
+
+	if (totalYears > 0) {
+		result.push(`${totalYears} year`)
+	}
+
+	if (totalMonths > 0) {
+		result.push(`${totalMonths} month`)
+	}
+
+	if (remainingDaysInMonth > 0) {
+		result.push(`${remainingDaysInMonth} days`)
+	}
+
+	return result.join(', ')
 }
